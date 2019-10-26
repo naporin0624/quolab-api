2439f4597e4273b20424037962e2b89b084acdfe
wally <u.str.gm@gmail.com>
2019-10-27 02:00:03 +0900

    feat: roomにlabIdを自動で入れるようにした

--- src/room/create-room/create-room.service.ts
+++ src/room/create-room/create-room.service.ts
@@ -4,13 +4,13 @@ import { InjectModel } from "@nestjs/mongoose";
 
 import { Room } from "../../types/entities/room.interface";
 import { RoomDto } from "../../types/dto/room.dto";
+import { User} from '../../types/entities/user.interface'
 @Injectable()
 export class CreateRoomService {
   constructor(@InjectModel("room") private readonly roomModel: Model<Room>) {}
 
-  createRoom(roomDto: Partial<RoomDto>) {
-    // TODO: ユーザーの所属研究室を自動で取得してあげる
-    roomDto.labId = "5db3e3278ebf8524b1f28337";
+  createRoom(roomDto: Partial<RoomDto>, user: User) {
+    roomDto.labId = user.labId;
     const createdRoom = new this.roomModel(roomDto);
     return createdRoom.save();
   }
--- src/room/room.controller.ts
+++ src/room/room.controller.ts
@@ -1,15 +1,25 @@
-import { Controller, Post, Body, UseGuards } from "@nestjs/common";
+import { Controller, Post, Body, UseGuards, Request, HttpStatus, HttpException } from "@nestjs/common";
 import { CreateRoomService } from "./create-room/create-room.service";
+import { UserService } from '../user/user.service'
 import { RoomDto } from "../types/dto/room.dto";
 import { AuthGuard } from "@nestjs/passport";
 
 @Controller("room")
 export class RoomController {
-  constructor(private readonly createRoomService: CreateRoomService) {}
+  constructor(private readonly createRoomService: CreateRoomService, private readonly userService: UserService) {}
 
   @UseGuards(AuthGuard("jwt"))
   @Post()
-  createRoom(@Body() roomDto: Partial<RoomDto>) {
-    return this.createRoomService.createRoom(roomDto);
+  async createRoom(
+    @Request() res: any,
+    @Body() roomDto: Partial<RoomDto>) {
+    const user = await this.userService.findOne(res.user.email)
+    if (!user.labId) {
+      throw new HttpException(
+        `please join is lab`,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    return this.createRoomService.createRoom(roomDto, user);
   }
 }
