becce9f94321e8d3546b49114d9f9f70751b300a
naporin0624 <naporin0624@gmail.com>
2019-10-27 04:17:19 +0900

    add 9hoours

--- src/visialization/visialization.service.ts
+++ src/visialization/visialization.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { UserService } from "../user/user.service";
 import { UserActivityService } from "../user-activity/user-activity.service";
+import { addHours } from "date-fns";
 
 @Injectable()
 export class VisializationService {
@@ -14,7 +15,7 @@ export class VisializationService {
     return browsingData
       .map(activity => {
         return {
-          createdAt: activity.createdAt,
+          createdAt: addHours(activity.createdAt, 9),
           url: activity.data.url,
           title: activity.data.title,
           domain: activity.data.url && new URL(activity.data.url).host,
