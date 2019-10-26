7c7bd7cc371499882353a157b10fed4918d0cb16
naporin0624 <naporin0624@gmail.com>
2019-10-26 11:32:37 +0900

    add sample CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { MongooseModule } from "@nestjs/mongoose";
+import { PlaygroundModule } from "./playground/playground.module";
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      "mongodb://ds139768.mlab.com:39768/heroku_x69xrrcc",
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        user: "heroku_x69xrrcc",
+        pass: "khfa8gd1cleocbkqt5c7mt5d7u",
+      },
+    ),
+    PlaygroundModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
