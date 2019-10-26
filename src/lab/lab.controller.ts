8b56bbec26cc9cba6f20415776eafcc68a394cd1
naporin0624 <naporin0624@gmail.com>
2019-10-26 17:34:24 +0900

    add jwt

--- src/lab/lab.controller.ts
+++ src/lab/lab.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Post, Body } from "@nestjs/common";
+import { Controller, Post, Body, UseGuards } from "@nestjs/common";
 import { LabDto } from "../types/dto/Lab.dto";
 import { CreateLabService } from "./create-lab/create-lab.service";
+import { AuthGuard } from "@nestjs/passport";
 
 @Controller("lab")
 export class LabController {
   constructor(private readonly createLabService: CreateLabService) {}
 
-  // TODO: ログイン専用
+  @UseGuards(AuthGuard("jwt"))
   @Post()
   createLab(@Body() createLabRequestDto: Partial<LabDto>) {
     return this.createLabService.createLab(createLabRequestDto);
--- src/room/room.controller.ts
+++ src/room/room.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Post, Body } from "@nestjs/common";
+import { Controller, Post, Body, UseGuards } from "@nestjs/common";
 import { CreateRoomService } from "./create-room/create-room.service";
 import { RoomDto } from "../types/dto/room.dto";
+import { AuthGuard } from "@nestjs/passport";
+
 @Controller("room")
 export class RoomController {
   constructor(private readonly createRoomService: CreateRoomService) {}
 
-  // TODO: ログイン専用
+  @UseGuards(AuthGuard("jwt"))
   @Post()
   createRoom(@Body() roomDto: Partial<RoomDto>) {
     return this.createRoomService.createRoom(roomDto);
