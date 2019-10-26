8f8cd40903abb8c0eea928446665aa1829bab03e
wally <u.str.gm@gmail.com>
2019-10-27 06:39:52 +0900

    chore: add dto, end point

--- src/slack-config/slack-config.controller.ts
+++ src/slack-config/slack-config.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards, Request, Post } from '@nestjs/common';
+import { SlackConfigService } from './slack-config.service';
+import { AuthGuard } from '@nestjs/passport';
+import { SlackConfigDto } from '@/types/dto/slackConfig.dto';
 
 @Controller('slack-config')
-export class SlackConfigController {}
+export class SlackConfigController {
+  constructor(private readonly slackConfigService: SlackConfigService){}
+
+  @UseGuards(AuthGuard('jwn'))
+  @Post()
+  async registerSlackConfig(@Request() slackConfigDto: SlackConfigDto){
+    return null
+  }
+}
