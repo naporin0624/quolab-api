7743ee296d42d36e1cca094a6488f21e7f7fb75b
naporin0624 <naporin0624@gmail.com>
2019-10-27 12:22:49 +0900

    ok

--- src/room/room.module.ts
+++ src/room/room.module.ts
@@ -4,7 +4,7 @@ import { RoomController } from "./room.controller";
 import { CreateRoomService } from "./create-room/create-room.service";
 import { RoomSchema } from "../types/schemas/room.schemas";
 import { UserModule } from "../user/user.module";
-import { RoomService } from './room.service';
+import { RoomService } from "./room.service";
 
 @Module({
   imports: [
@@ -18,5 +18,6 @@ import { RoomService } from './room.service';
   ],
   controllers: [RoomController],
   providers: [CreateRoomService, RoomService],
+  exports: [RoomService, CreateRoomService],
 })
 export class RoomModule {}
--- src/visialization/visialization.module.ts
+++ src/visialization/visialization.module.ts
@@ -3,9 +3,18 @@ import { VisializationService } from "./visialization.service";
 import { UserModule } from "../user/user.module";
 import { UserActivityModule } from "../user-activity/user-activity.module";
 import { VisializationController } from "./visialization.controller";
+import { EnvDataModule } from "../env-data/env-data.module";
+import { LabModule } from "../lab/lab.module";
+import { RoomModule } from "../room/room.module";
 
 @Module({
-  imports: [UserModule, UserActivityModule],
+  imports: [
+    UserModule,
+    UserActivityModule,
+    EnvDataModule,
+    LabModule,
+    RoomModule,
+  ],
   controllers: [VisializationController],
   providers: [VisializationService],
 })
