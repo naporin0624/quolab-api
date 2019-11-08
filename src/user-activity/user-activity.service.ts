99f89f0d9fb855cbf0ca9aaba9b32a5f4eddf4cf
naporin0624 <naporin0624@gmail.com>
2019-11-08 21:07:01 +0900

    :sparkles: weeklyreport v2

--- src/user-activity/user-activity.service.ts
+++ src/user-activity/user-activity.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
-import { UserActivityData } from "src/types/entities/userActivityData.interface";
-import { subHours } from "date-fns";
+import { UserActivityData } from "../types/entities/userActivityData.interface";
+import { subHours, subWeeks } from "date-fns";
+export interface Category {
+  survey: number;
+  writing: number;
+  implementation: number;
+  break: number;
+}
 
 @Injectable()
 export class UserActivityService {
@@ -48,5 +54,43 @@ export class UserActivityService {
         },
       },
     ]);
+  }
+
+  async fetchWeeklyCategories(userId: string) {
+    const startTime = subWeeks(new Date(), 1);
+    const endTime = new Date();
+
+    const items: UserActivityData[] = await this.userActivityModel.aggregate([
+      {
+        $match: {
+          userId: userId,
+          category: { $ne: undefined },
+          createdAt: {
+            $gte: startTime,
+            $lt: endTime,
+          },
+        },
+      },
+    ]);
+    const categories = [
+      "survey",
+      "writing",
+      "implementation",
+      "break",
+    ] as const;
+    const categoryCount = categories.map(
+      category => items.filter(item => item.category === category).length,
+    );
+    const data = categoryCount.map(
+      c => (c * 100) / categoryCount.reduce((a, b) => a + b),
+    );
+    return {
+      labels: categories,
+      datasets: [
+        {
+          data,
+        },
+      ],
+    };
   }
 }
--- src/weekly-report/weekly-report.controller.ts
+++ src/weekly-report/weekly-report.controller.ts
@@ -3,18 +3,20 @@ import { UserService } from "../user/user.service";
 import { AuthGuard } from "@nestjs/passport";
 import { RequestWithUser } from "../types/";
 import { WeeklyReportService } from "./weekly-report.service";
+import { UserActivityService } from "../user-activity/user-activity.service";
 
 @UseGuards(AuthGuard("jwt"))
 @Controller("weekly-report")
 export class WeeklyReportController {
   constructor(
     private readonly userService: UserService,
+    private readonly userActivityService: UserActivityService,
     private readonly weeklyReportService: WeeklyReportService,
   ) {}
 
   @Get()
   async index(@Request() req: RequestWithUser) {
     const user = await this.userService.findOne(req.user.email);
-    return this.weeklyReportService.weeklyDataList(user);
+    return this.userActivityService.fetchWeeklyCategories(user._id.toString());
   }
 }
